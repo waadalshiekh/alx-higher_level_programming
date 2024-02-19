a442dcc46c72895076820022aa4e506df8dee411
Waad Alshiekh <waad9726@gmail.com>
2024-02-19 13:42:45 -0800

    t

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- a script that prints the full description of the table first_table from the database hbtn_0c_0
-mysql hbtn_0c_0 -e "
-SHOW CREATE TABLE first_table
-"
+-- show info in table without using describe
+SHOW CREATE TABLE first_table;
