9548b8e15467e98590214c29dec4fa479c522852
Waad Alshiekh <waad9726@gmail.com>
2024-03-15 12:15:12 -0700

    o

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
+// It prints 3 lines
 
-const first = 'C is fun';
-const second = 'Python is cool';
-const third = 'Javascript is amazing';
-
-console.log(first);
-console.log(second);
-console.log(third);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
