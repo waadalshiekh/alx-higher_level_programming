cfb78f9fded379e49a99a89d8147b73349777b64
Waad Alshiekh <waad9726@gmail.com>
2024-03-28 15:04:43 -0700

    d

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" Use urllib to fetch website """
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
-        print('Body response:')
-        print('\t- type:', type(body))
-        print('\t- content:', body)
-        print('\t- utf8 content:', body.decode('UTF-8'))
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
