015aefde96af684b6ed69da18659ccabc34a84a5
Waad Alshiekh <waad9726@gmail.com>
2024-03-30 17:09:07 -0700

    e

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"Module that fetches https://alx-intranet.hbtn.io/status"
+"""  fetches https://intranet.hbtn.io/status  """
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        r = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
